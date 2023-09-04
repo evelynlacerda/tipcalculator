c8eb625e4ba0a8b209617c8a36733282bf776fdb
Evelyn Lacerda <evelyndslacerda@gmail.com>
2023-09-04 14:12:46 -0300

    :sparkles: feat: start create script

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+const bill = document.querySelector('[data-bill]');
+const tips = document.querySelectorAll('[data-tip-button]');
+const customTip = document.querySelector('[data-custom-tip]');
+const people = document.querySelector('[data-people]');
+const amountValue = document.querySelector('[data-amount-value]');
+const totalValue = document.querySelector('[data-total-value]');
+const resetButton = document.querySelector('[data-reset-button]');
+
